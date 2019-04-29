95198f70c1cc6c37727b0ba9341d8e7a0df9280c
Elena Rolskaya <e.rolskaya@kv.beets>
2019-04-29 19:58:25 +0300

    git shit done v.2

--- code.js
+++ code.js
@@ -2,21 +2,24 @@ function moneyConverter(valNum) {
     document.getElementsByName("check").forEach(function(item) {  // подключись к чек-боксам
         item.addEventListener('click', function() { // как только на одну из чек-боксов нажмут
             if (true && item.id === "USD") { // и id чек-бокса будет USD
-                usdVal = (valNum * 0.27).toFixed(2)
-                document.getElementById("outputrub").innerHTML = usdVal + " USD"
+                let convertVal = (valNum * 0.27).toFixed(2)
+                document.getElementById("outputrub").innerHTML = convertVal + " USD"
             }
             else if (true && item.id === "EUR") { // и id чек-бокса будет EUR
-                eurVal = (valNum * 0.3).toFixed(2)
-                document.getElementById("outputrub").innerHTML = eurVal + " EUR"
+                convertVal = (valNum * 0.3).toFixed(2)
+                document.getElementById("outputrub").innerHTML = convertVal + " EUR"
             }
             else if (true && item.id === "RUB") { // и id чек-бокса будет RUB
-                document.getElementById("outputrub").innerHTML = valNum * 30 + " RUB"
+                convertVal = (valNum * 30).toFixed(2)
+                document.getElementById("outputrub").innerHTML = convertVal + " RUB"
             }
             else if (true && item.id === "AUD") { // и id чек-бокса будет AUD
-                document.getElementById("outputrub").innerHTML = valNum * 0.18 + " AUD"
+                convertVal = (valNum * 0.18).toFixed(2)
+                document.getElementById("outputrub").innerHTML = convertVal + " AUD"
             }
             else if (true && item.id === "NPR") { // и id чек-бокса будет NPR
-                document.getElementById("outputrub").innerHTML = valNum * 24 + " NPR"
+                convertVal = (valNum * 24).toFixed(2)
+                document.getElementById("outputrub").innerHTML = convertVal + " NPR"
             } 
         })
     })
